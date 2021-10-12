0aa0525fbb1e2cfff1158ae1099a36247d84a299
JL <joshua.lung@student.htldornbirn.at>
2021-10-12 17:26:38 +0200

    webpack reloading using webpack-ext-reloader

---
+++ app/webpackExtension3/src/background/index.ts
@@ -0,0 +1,1 @@
+console.log('Background Script: "Hello World"');
---
+++ app/webpackExtension3/webpack.config.js
@@ -0,0 +1,79 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ExtReloader = require("webpack-ext-reloader");
+//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+  cache: false,
+  entry: {
+    background: "./src/background/index.ts",
+    content: "./src/content/index.ts",
+    popup: "./src/popup/index.ts",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new ExtReloader({
+      manifest: path.resolve(__dirname, "./src/manifest.json"),
+      reload: true,
+      port: 8001,
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, "./src/manifest.json") }],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/popup/index.html"),
+      filename: "popup.html",
+    }),
+  ],
+};
+
+module.exports = config;
