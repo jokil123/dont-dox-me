8d4af802c745612a6966a3a20c42274d17a59786
JL <joshua.lung@student.htldornbirn.at>
2021-10-19 18:45:30 +0200

    forgot to stage

--- app/extension/src/content/loadSettings.ts
+++ app/extension/src/content/loadSettings.ts
@@ -1,7 +1,43 @@
 import * as storage from "./asyncChromeStorage";
 
-export interface settings {}
+export interface setting {
+  text: string;
+}
 
-export const loadSettings = async (): settings[] => {
-  let storageBuckets: string[] = await storage.get("storageBuckets");
+export const loadSettings = async (): Promise<setting[]> => {
+  await storage.set({ storageBuckets: ["a", "b"] });
+  await storage.set({ a: { text: "aText" } });
+  await storage.set({ b: { text: "bText" } });
+  let storageBuckets: string[] = (await storage.get("storageBuckets"))[
+    "storageBuckets"
+  ];
+
+  let settings: setting[] = (
+    await Promise.all(
+      storageBuckets.map((bucket) => {
+        return storage.get(bucket);
+      })
+    )
+  ).map((setting) => {
+    return Object.values(setting)[0];
+  });
+
+  return settings;
+};
+
+export const deleteSettings = async (settingIds: string[]): Promise<void> => {
+  let storageBuckets: string[] = (await storage.get("storageBuckets"))[
+    "storageBuckets"
+  ];
+
+  storageBuckets = storageBuckets.filter((bucket) => {
+    return !settingIds.includes(bucket);
+  });
+
+  await Promise.all([
+    ...settingIds.map((settingId) => {
+      return storage.remove(settingId);
+    }),
+    storage.set({ storageBuckets: storageBuckets }),
+  ]);
 };
