ab67442df15a8eb6a26448ad52638f38eb09a47b
jo_kil <jo.kil.plays@gmail.com>
2021-10-31 20:00:31 +0100

    censor ignores nodes without parent, refactoring

--- app/extension/src/content/censor/index.ts
+++ app/extension/src/content/censor/index.ts
@@ -3,10 +3,10 @@ import "./censorStyle.scss";
 import { getTextNodesIn } from "./getTextNodes";
 import { loadSettings, overwriteSettings } from "../../util/manageSettings";
 import { settings } from "../../util/settingsInterface";
-import { filterTextMutations } from "./filterMutations";
 import { censorNodes } from "./censorNodes";
 import * as pageHide from "./hideWebpage";
 import { filterNodes } from "./censorIgnore";
+import { setupObserver } from "./pageMutationHandler";
 
 const main = async () => {
   let settings: settings = await loadSettings();
@@ -23,40 +23,7 @@ const main = async () => {
 
   pageHide.show();
 
-  let observeSettings = {
-    subtree: true,
-    characterData: true,
-    attributes: true,
-    childList: true,
-  };
-
-  let characterDataObserver = new MutationObserver((m) => {
-    characterDataObserver.disconnect();
-
-    let filteredTextMutations = filterTextMutations(m);
-
-    let hiddenElements: HTMLElement[] = [];
-
-    filteredTextMutations.forEach((e) => {
-      if (e.parentElement) {
-        hiddenElements.push(e.parentElement);
-        pageHide.hide(e.parentElement);
-        console.log();
-      }
-    });
-
-    filteredTextMutations.forEach((m) => {
-      censorNodes(getTextNodesIn(m, true), settings.rules);
-    });
-
-    hiddenElements.forEach((e) => {
-      pageHide.show(e);
-    });
-
-    characterDataObserver.observe(document.body, observeSettings);
-  });
-
-  characterDataObserver.observe(document.body, observeSettings);
+  setupObserver(settings);
 };
 
 main();
---
+++ app/extension/src/content/censor/pageMutationHandler.ts
@@ -0,0 +1,44 @@
+import { censorNodes } from "./censorNodes";
+import { filterTextMutations } from "./filterMutations";
+import { getTextNodesIn } from "./getTextNodes";
+import { settings } from "../../util/settingsInterface";
+import * as pageHide from "./hideWebpage";
+
+const observeSettings = {
+  subtree: true,
+  characterData: true,
+  attributes: true,
+  childList: true,
+};
+
+export const setupObserver = (settings: settings) => {
+  let observer = new MutationObserver((m) => {
+    observer.disconnect();
+
+    let filteredTextMutations = filterTextMutations(m);
+
+    let hiddenElements: HTMLElement[] = <HTMLElement[]>filteredTextMutations
+      .map((n) => {
+        return n.parentElement;
+      })
+      .filter((n) => {
+        return n;
+      });
+
+    hiddenElements.forEach((n) => {
+      pageHide.hide(n);
+    });
+
+    filteredTextMutations.forEach((m) => {
+      censorNodes(getTextNodesIn(m, true), settings.rules);
+    });
+
+    hiddenElements.forEach((n) => {
+      pageHide.show(n);
+    });
+
+    observer.observe(document.body, observeSettings);
+  });
+
+  observer.observe(document.body, observeSettings);
+};
